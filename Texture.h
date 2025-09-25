d1b27082cf2de1b94bbd9064cde2ef9e68a627d5
Jay Lee <lee03493@umn.edu>
2025-09-24 19:05:40 -0500

    update

--- Texture.h
+++ Texture.h
@@ -7,7 +7,7 @@ class Texture {
 public:
     Texture(int width, int height, unsigned char* img);
     void use(unsigned int shaderProgram);
-    void copy(int width, int height, unsigned char* img);
+    void copypToGPU(int width, int height, unsigned char* img);
 
 private:
     unsigned int texture;
