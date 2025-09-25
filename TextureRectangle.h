a5becbb02c6649d2187ef62b1a98879d52149ae3
Jay Lee <lee03493@umn.edu>
2025-09-24 23:21:35 -0500

    update

--- TextureRectangle.h
+++ TextureRectangle.h
@@ -10,22 +10,22 @@ public:
     void draw(unsigned int shaderProgram);
 private:
     unsigned int VBO, VAO, EBO;
-    float vertices[12] = {
-         1.0f,  1.0f, 0.0f,  // top right
-         1.0f, -1.0f, 0.0f,  // bottom right
-        -1.0f, -1.0f, 0.0f,  // bottom left
-        -1.0f,  1.0f, 0.0f   // top left 
-    };
-    float coords[8] = {
-         1.0f,  0.0f,  // top right
-         1.0f, 1.0f,  // bottom right
-        0.0f, 1.0f,  // bottom left
-        0.0f,  0.0f   // top left 
-    };
-    unsigned int indices[6] = {  // note that we start from 0!
-        0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
-    };
+    // float vertices[12] = {
+    //      1.0f,  1.0f, 0.0f,  // top right
+    //      1.0f, -1.0f, 0.0f,  // bottom right
+    //     -1.0f, -1.0f, 0.0f,  // bottom left
+    //     -1.0f,  1.0f, 0.0f   // top left 
+    // };
+    // float coords[8] = {
+    //      1.0f,  0.0f,  // top right
+    //      1.0f, 1.0f,  // bottom right
+    //     0.0f, 1.0f,  // bottom left
+    //     0.0f,  0.0f   // top left 
+    // };
+    // unsigned int indices[6] = {  // note that we start from 0!
+    //     0, 1, 3,  // first Triangle
+    //     1, 2, 3   // second Triangle
+    // };
 };
 
 }
