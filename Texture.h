c8cd2769b995a7f1b1e5f3fa0192f63638b54d80
Jay Lee <lee03493@umn.edu>
2025-09-24 19:18:00 -0500

    update

--- Texture.h
+++ Texture.h
@@ -5,6 +5,7 @@ namespace csci3081{
 
 class Texture {
 public:
+    Texture();
     Texture(int width, int height, unsigned char* img);
     void use(unsigned int shaderProgram);
     void copyToGPU(int width, int height, unsigned char* img);
