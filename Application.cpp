7efe8a43ffceb52f69e27b9a5678cf3c5464a032
Jay Lee <lee03493@umn.edu>
2025-09-26 22:24:50 -0500

    update

--- Application.cpp
+++ Application.cpp
@@ -82,16 +82,16 @@ int Application::run() {
     // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     // GLFWwindow* window = glfwCreateWindow(width, height, "Image Editor", NULL, NULL);
-    if (window == NULL)
+    if (appWindow.getWindow == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, cursor_position_callback);
-    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwMakeContextCurrent(appWindow.getWindow());
+    glfwSetFramebufferSizeCallback(ppWindow.getWindow(), framebuffer_size_callback);
+    glfwSetCursorPosCallback(ppWindow.getWindow(), cursor_position_callback);
+    glfwSetMouseButtonCallback(ppWindow.getWindow(), mouse_button_callback);
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
