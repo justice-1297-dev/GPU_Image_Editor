f1f65caa84a2a2c54d3a8e8f43a955ee187d868a
Jay Lee <lee03493@umn.edu>
2025-09-26 21:45:46 -0500

    d

--- Texture.cpp
+++ Texture.cpp
@@ -5,14 +5,14 @@
 
 namespace csci3081{
 
-Texture::Texture(const Image& image){
+Texture::Texture(width, height, const Image& image){
     glGenTextures(1, &texture);  
     glBindTexture(GL_TEXTURE_2D, texture);  
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.getWidth(), image.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, image.getImg());
     glGenerateMipmap(GL_TEXTURE_2D);
 }
 
